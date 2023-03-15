a7ea8b75891a13de9f4a3bc24152dbaa2bbfce8a
SergiFont <sfontsmx2010@gmail.com>
2023-03-15 18:40:04 +0100

    implementado el token en el controlador para las partidas

--- app/controllers/games.js
+++ app/controllers/games.js
@@ -1,9 +1,21 @@
 const { Games } = require('../models'); // Import the Game model from Sequelize
+const { Players } = require('../models');
 const {sendError, sendResponse} = require('../utils/serverReplys.js')
 
 exports.getGames = async (req, res) => {
+  const userId = req.user.id
   try {
-    const games = await Games.findAll(); // Retrieve all games from the database using Sequelize
+    const player = await Players.findOne({
+      where : {
+        userId : userId
+      }
+    })
+    console.log(player)
+    const games = await Games.findAll({
+      where : {
+        playerId : player.id
+      }
+    }); // Retrieve all games from the database using Sequelize
     sendResponse(res, 200, games) // Return the games as JSON
   } catch (err) {
     console.error(err);
@@ -12,9 +24,18 @@ exports.getGames = async (req, res) => {
 };
 
 exports.createGame = async (req, res) => {
-  const { name } = req.query;
+  const userId = req.user.id
   try {
-    const game = await Games.create({ name }); // Create a new game record in the database using Sequelize
+    const dice1 = Math.ceil(Math.random() * 6)
+    const dice2 = Math.ceil(Math.random() * 6)
+    const victory = dice1 + dice2 === 7 ? true : false
+    const player = await Players.findOne({
+      where : {
+        userId : userId
+      }
+    })
+    const playerId = player.id
+    const game = await Games.create({dice1, dice2, victory, playerId}); // Create a new game record in the database using Sequelize
     sendResponse(res, 201, game); // Return the newly created game as JSON
   } catch (err) {
     console.error(err);
@@ -22,11 +43,17 @@ exports.createGame = async (req, res) => {
   }
 };
 
-exports.deleteGame = async (req, res) => {
-  const { name } = req.query; // Extract the game ID from the request parameters
+exports.deleteGames = async (req, res) => {
+  const userId = req.user.id
+  let player = await Players.findOne({
+    where : {
+      userId : userId
+    }
+  })
+  player = player.id
   try {
-    await Games.destroy({ where: { name } }); // Delete the game record from the database using Sequelize
-    sendResponse(res, 204)
+    await Games.destroy({ where: { playerId : player } }); // Delete the game record from the database using Sequelize
+    sendResponse(res, 200, { message: 'Games deleted succesfully' })
   } catch (err) {
     console.error(err);
     res.status(500).json({ error: 'Server error' }); // Return an error response if something goes wrong
--- app/routes/games.js
+++ app/routes/games.js
@@ -1,10 +1,10 @@
 const express = require('express')
 const router = express.Router()
-const { getGames, createGame, deleteGame} = require('./../controllers/games.js')
+const { getGames, createGame, deleteGames} = require('./../controllers/games.js')
 
 router.route('/:name?')
     .post(createGame)
-    .delete(deleteGame)
+    .delete(deleteGames)
     .get(getGames)
 
   module.exports = router
